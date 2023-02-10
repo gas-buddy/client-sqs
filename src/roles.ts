2c9a1bc647cd7f8ded53adc5d4739750fa2bec52
Max Metral <max@pyralis.com>
2023-02-10 16:33:57 -0500

    Role is not tied in yet, but fix code

--- src/roles.ts
+++ src/roles.ts
@@ -1,13 +1,13 @@
-import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
+import { GetCallerIdentityCommand, STS } from '@aws-sdk/client-sts';
 
 export async function assumeRole(role?: string) {
   if (!role) {
     return undefined;
   }
-  const sts = new STSClient({ apiVersion: '2011-06-15' });
+  const sts = new STS({ apiVersion: '2011-06-15' });
   const { Arn: actualRoleArn } = await sts.send(new GetCallerIdentityCommand({}));
   if (!actualRoleArn?.includes(role)) {
-    throw new Error(`Role is ${actualRoleArn} expecting to contain ${config.assumedRole}`);
+    throw new Error(`Role is ${actualRoleArn} expecting to contain ${role}`);
   }
   return actualRoleArn;
 }
